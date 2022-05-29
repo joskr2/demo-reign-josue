6baf6d2f9945de0b1e328d64059df9e3de94cc31
Josue Patricio Retamozo Vargas <josretamozo@gmail.com>
2022-05-29 12:03:01 -0500

    feat(card): style card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,12 +1,51 @@
 import './style.css';
 import FavIcon from '../../assets/images/favorite.png'
 import NoFavIcon from '../../assets/images/no-favorite.png';
-import LightClockIcon from '../../assets/images/time-light.png';
 import DarkClockIcon from '../../assets/images/time-dark.png';
+import { useState } from 'react';
 
 const Card = () => {
+
+  const [isSelected, setIsSelected] = useState(false);
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const toggleFavorite = () => setIsFavorite(prevState => !prevState);
+  const toggleSelected = () => setIsSelected(prevState => !prevState);
+
+  const handleOpenLink = (url: string) => {
+    toggleSelected();
+
+    // if(isSelected){
+    //   window.open(url, '_blank')
+    // }
+  }
+
+  const saveToFavoritesToLocalStorage = (item: any) => {
+    console.log(`Save to favorites ${item}`);
+    // const favorites = JSON.parse(localStorage.getItem('favorites') || '[]');
+    // const isFavorite = favorites.some(item => item.id === id);
+
+    // if (isFavorite) {
+    //   const newFavorites = favorites.filter(item => item.id !== id);
+    //   localStorage.setItem('favorites', JSON.stringify(newFavorites));
+    // }
+    // else {
+    //   localStorage.setItem('favorites', JSON.stringify([...favorites, { id }]));
+    // }
+
+    if (isFavorite) {
+      // save to local storage
+      console.log('save to local storage');
+      toggleFavorite();
+    } else {
+      // remove from local storage
+      toggleFavorite();
+      console.log('remove from local storage');
+    }
+  }
+
   return (
-    <div className="card">
+    <div onClick={() => handleOpenLink('')} className={isSelected ? 'card card-selected' : 'card'}>
       <div className="card-body">
         <div className="card-content">
           <div className="card-time-info">
@@ -20,7 +59,7 @@ const Card = () => {
           </div>
         </div>
         <div className='card-icon-container'>
-          <img className="card-icon" src={NoFavIcon} width='24px' height='22px' />
+          <img onClick={saveToFavoritesToLocalStorage} className="card-icon" src={!isFavorite ? NoFavIcon : FavIcon} width='24px' height='22px' />
         </div>
       </div>
     </div>
